b06cdebc67b6a50f8089dae930abc31e330bce60
Chaitanya <chaitanyachordiya@gmail.com>
2026-02-25 09:53:17 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("this is from iccs bca science 🚀");
+  res.send("this is from iccs bca ");
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

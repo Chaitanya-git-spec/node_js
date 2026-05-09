8f17a7f63a5b985e263c06c5c56eaf74b46f0837
chaitanya <54612528+Chaitanya-git-spec@users.noreply.github.com>
2026-05-09 12:02:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(" New changes pushed from GITHUN ");
+  res.send(" Hello Team devops12 ");
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
